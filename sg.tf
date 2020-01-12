92aaa9dd9adfd40ec7c79376d2fa37a3d644ba09
muratto33 <muratkilic33@yahoo.com>
2020-01-12 16:43:01 -0600

    add

--- sg.tf
+++ sg.tf
@@ -1,17 +1,21 @@
-resource "aws_security_group" "allow_tls" { 
-  name        = "allow_ssh" 
-  description = "Allow shh inbound traffic" 
-  vpc_id      = "vpc-79226b03" 
-  ingress { 
-    from_port   = 443 
-    to_port     = 443 
-    protocol    = "tcp" 
-    cidr_blocks = ["0.0.0.0/0"]   
-} 
-egress { 
-    from_port       = 0 
-    to_port         = 0 
-    protocol        = "-1" 
-    cidr_blocks     = ["0.0.0.0/0"] 
-  } 
-} 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-1471ad6e"
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}

a5929a2b21bbaa782fbe442e0fde2a2d6508e890
muratto33 <muratkilic33@yahoo.com>
2020-01-12 13:02:07 -0600

    add

--- intance.tf
+++ intance.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" { 
   ami           = var.ami
   instance_type = "t2.micro" 
+  instance_state               =  stopped
+  key_name                     =  deployer
  
 tags = { 
     Name = "HelloWorld" 

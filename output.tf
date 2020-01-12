2f37e6a5f175687beca7124f7b1b8dacab72b8e4
muratto33 <muratkilic33@yahoo.com>
2020-01-12 16:07:09 -0600

    add output

---
+++ output.tf
@@ -0,0 +1,27 @@
+output "bucket_name" {
+    value = aws_s3_bucket.b.bucket
+}
+
+output "sec_group" {
+    value = aws_security_group.allow_ssh.name
+}
+
+output "key_name" {
+    value = aws_key_pair.deployer.key_name
+}
+
+
+
+output "wordpress" {
+    value = aws_route53_record.www.name
+}
+output "wordpress_username" {
+    value = "some_username"
+}
+output "wordpress_password" {
+    value = "password"
+}
+
+output "IPs" {
+    value = aws_instance.web.*.public_ip
+}

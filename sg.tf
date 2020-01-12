01dc80f539e3d74b711d5e2939524b388f893032
muratto33 <muratkilic33@yahoo.com>
2020-01-12 17:01:33 -0600

    add vpc

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-1471ad6e"
+  vpc_id      = "vpc-79226b03"
 
 
   ingress {

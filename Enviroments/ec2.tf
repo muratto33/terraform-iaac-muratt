6dcb32dba1fa1baf1224c5227f5263d31e7151f0
muratto33 <muratkilic33@yahoo.com>
2020-01-14 15:08:17 -0600

    add

--- Enviroments/ec2.tf
+++ Enviroments/ec2.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "instance" {
     ami = var.ami
     instance_type =  var.instance_type
     key_name =   var.key_name
-    
+    tags = {
+        Name = var.environment
+    }
 } 
